608493af2eaa196d402f480b1996bc372ffa477b
Crystal <crystalliang882@gmail.com>
2019-06-25 15:30:57 -0700

    idk

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'home/index'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
