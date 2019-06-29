39976744715974430b335bca8e3692e9eefbe1e8
jusung <jusunglee1459@gmail.com>
2019-06-29 16:55:21 -0700

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'pages/welcome'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#welcome'
   get 'users/new'
   get 'pages/index';
-  get  '/signup',  to: 'users#new'
+  get '/signup',    to: 'users#new'
+  get '/login',     to: 'sessions#new'
+  post '/login',    to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
