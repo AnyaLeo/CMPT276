cff97169ccd67885391a289aa4c76c2ab1c61ae8
Crystal <crystalliang882@gmail.com>
2019-06-30 19:44:27 -0700

    can draw on board (can't erase+ notes dont spawn in same spot)

--- app/assets/javascripts/board.js
+++ app/assets/javascripts/board.js
@@ -1,12 +1,19 @@
 $(document).ready(function(){
+    var canvas = new fabric.Canvas('drawingCanvas');
+  canvas.isDrawingMode = false;
+  canvas.freeDrawingBrush.width = 5;
+  canvas.freeDrawingBrush.color = "black";
+  canvas.setHeight(3920);
+  canvas.setWidth(3970);
+
   var deleteStatus = false;
 
   //add new text note on click
   $('#add').click(function(){
     deleteStatus = false;
     var newDiv = '<div class="movable resizable" contenteditable="true">New note</div>';
-    $("#notesContainer").append(newDiv);
-    $(".movable").draggable({containment : [0, 50, 4000, 4000] });
+    $('#notesContainer').append(newDiv);
+    $(".movable").draggable({containment : [30, 80, 4000, 4000] });
     $('.resizable').resizable();
   });
 
@@ -21,8 +28,8 @@ $(document).ready(function(){
 
     $('#imgForm').trigger("reset");
     var newDiv = '<div class="movable"><img class="resizable" src="' + imgSrc + '" alt="Image"></div>';
-    $("#notesContainer").append(newDiv);
-    $('.movable').draggable();
+    $('body').append(newDiv);
+    $('.movable').draggable({containment : [30, 80, 4000, 4000] });
     $('.resizable').resizable();
   });
 
@@ -34,6 +41,16 @@ $(document).ready(function(){
   $('#notesContainer').click(function(e){
     if (deleteStatus) {
       $(e.target).remove();
+    }
+  });
+
+  //drawing
+  $('#draw').click(function(){
+    if (canvas.isDrawingMode){
+      canvas.isDrawingMode = false;
+    }
+    else {
+      canvas.isDrawingMode = true;
     }
   });
 
