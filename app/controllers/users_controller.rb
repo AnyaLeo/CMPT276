ab4aa757ec2d514096cb03378151d6698d875c36
GroovyRoom <l.chang.suk@gmail.com>
2019-07-01 09:02:47 +0000

    Sign-up done with ruby 2.5.5 with Postgre

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,19 @@
 class UsersController < ApplicationController
-  
+
   def show
     @user = User.find(params[:id])
   end
-  
+
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to @user
+      # Handle a successful save.
       flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
