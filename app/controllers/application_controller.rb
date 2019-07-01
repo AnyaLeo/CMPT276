34d75bc6a6f67f67e60e914f091dc7198882ffad
GroovyRoom <l.chang.suk@gmail.com>
2019-07-01 00:26:58 +0000

    Add a Static Pages controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  root 'application#hello'
+    def hello
+     render html: "hello, world!"
+    end
 end
