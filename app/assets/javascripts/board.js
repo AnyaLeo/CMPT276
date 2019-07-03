55ab45e021dba5016ced1b09ecddffdbf2c694f6
Crystal <crystalliang882@gmail.com>
2019-07-02 17:40:57 -0700

    add image bug fixed

--- app/assets/javascripts/board.js
+++ app/assets/javascripts/board.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
     deleteStatus = false;
     var newDiv = '<div class="movable resizable" contenteditable="true">New note</div>';
     $('#notesContainer').append(newDiv);
-    $(".movable").draggable({containment : [30, 80, 4000, 4000] }).css("position", "absolute");
+    $('.movable').draggable({containment : [30, 80, 4000, 4000] }).css("position", "absolute");
     $('.resizable').resizable();
   });
 
@@ -25,11 +25,10 @@ $(document).ready(function(){
   $('#imgLinkInput').click(function(){
     deleteStatus = false;
     var imgSrc = $('input[name=imgLink]').val();
-
     $('#imgForm').trigger("reset");
     var newDiv = '<div class="movable"><img class="resizable" src="' + imgSrc + '" alt="Image"></div>';
-    $('body').append(newDiv);
-    $('.movable').draggable({containment : [30, 80, 4000, 4000] });
+    $('#notesContainer').append(newDiv);
+    $('.movable').draggable({containment : [30, 80, 4000, 4000] }).css("position", "absolute");
     $('.resizable').resizable();
   });
 
