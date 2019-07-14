4bba057473a8a151c1dced29db454e98a8073038
GroovyRoom <l.chang.suk@gmail.com>
2019-07-14 01:21:54 +0000

    Fixed the page controller error

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,4 @@
 class PagesController < ApplicationController
-  #we need this so that the css is not messed up
-  # layout false;
-  # def index
-    # @board = Board.new
-    # @boards = Board.all
-    # @user = User.find_by_id(session["user_id"])
-  # end
 
   def home
   end
