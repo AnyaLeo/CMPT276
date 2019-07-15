7788521fc40041b076e3fd44ebd1599e174496e6
GroovyRoom <l.chang.suk@gmail.com>
2019-07-15 19:35:24 +0000

    Fixed the typo of welcom message in Sign-up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Welcome to the MULTIBRAIN!"
       redirect_to @user
     else
       render 'new'
