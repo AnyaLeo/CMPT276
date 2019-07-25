de15e5ede8fb89c33f36c65c4df343902e7882b7
Anya <ania.leonova@gmail.com>
2019-07-25 10:51:00 -0700

    Some more changes to the UI, won't be finished for the demo

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,8 +1,14 @@
 $(window).on('scroll', function(){
     if($(window).scrollTop()){
         $('#header').addClass('black');
+        $('#logo').addClass('white');
+        $('.navbar-custom').addClass('black');
+        //$('a').addClass('black');
     }
     else {
         $('#header').removeClass('black');
+        $('#logo').removeClass('white');
+        $('.navbar-custom').removeClass('black');
+        //$('a').removeClass('black');
     }
 });
